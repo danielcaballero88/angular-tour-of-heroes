e5ebf5171c8b8a4c3e2199a4af69fcc56a53cf73
Daniel Caballero <danielcaballero88@gmail.com>
2023-12-13 18:05:38 +0100

    Display list of heroes with *ngFor structural directive

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
 }
