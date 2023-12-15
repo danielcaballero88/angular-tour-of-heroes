f5af330e82ece7aeca508548112431709a02e61d
Daniel Caballero <danielcaballero88@gmail.com>
2023-12-15 23:12:21 +0100

    Add dashboard component with top heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component'
 import { HeroesComponent } from './heroes/heroes.component'
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }]
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 import { HeroesComponent } from './heroes/heroes.component'
 import { MessagesComponent } from './messages/messages.component'
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
