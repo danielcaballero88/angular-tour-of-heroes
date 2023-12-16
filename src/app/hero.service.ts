783a22807daeeee8827138fd7c0d805075bed173
Daniel Caballero <danielcaballero88@gmail.com>
2023-12-16 10:04:48 +0100

    Add error handling with rxjs pipe

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 
-import { Observable, of } from 'rxjs'
+import { Observable, catchError, of, tap } from 'rxjs'
 
 import { Hero } from 'src/app/hero'
 import { MessageService } from 'src/app/message.service'
@@ -28,11 +28,19 @@ export class HeroService {
     // Now we are fetching the heroes by making a request to the api (backend). This is being mocked
     // by the in-memory-data service which is using the in-memory-web-api of Angular.
     // https://github.com/angular/angular/tree/main/packages/misc/angular-in-memory-web-api
-    const heroes = this.http.get<Hero[]>(this.heroesUrl)
-    // Adding a message when fetching heroes
-    this.log('fetched heroes')
-    // Done
-    return heroes
+    return (
+      this.http
+        .get<Hero[]>(this.heroesUrl)
+        // Pipe for the response (I add this comment to get the .pipe in a newline).
+        .pipe(
+          // tap does something and passes the values along (it taps into the observable)
+          tap(heroes => this.log(`fetched ${heroes.length} heroes`)),
+          // if there is an error it is catched by this operator which takes a callback a parameter,
+          // so we pass the service generice error handler function, which returns a function that
+          // will handle the error.
+          catchError(this.handleError<Hero[]>('getHeroes', [])),
+        )
+    )
   }
 
   getHero(id: number): Observable<Hero> {
@@ -41,5 +49,26 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)! // eslint-disable-line @typescript-eslint/no-non-null-assertion
     this.log(`fetched hero id=${id}`)
     return of(hero)
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error) // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`)
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T)
+    }
   }
 }
